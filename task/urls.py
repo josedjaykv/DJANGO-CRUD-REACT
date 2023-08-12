f75fea6d9456ed4d12b77959a1794e62a06be9fe
josedjaykv <josedjaykv@gmail.com>
2023-08-12 18:49:56 -0500

    first commit

---
+++ task/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import routers
+from task import views
+
+#versionado de api
+router = routers.DefaultRouter()
+router.register(r'task', views.TaskView, 'task')
+
+urlpatterns = [
+    path('api/v1/', include(router.urls)),
+    path('docs/', include_docs_urls(title="Task API")),
+]
+
